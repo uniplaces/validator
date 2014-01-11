87cda1df0809f1387d1143933a9f429ba12dbb94
entering <entering@gmail.com>
2014-01-11 22:35:46 +0000

    Add ValidatorNumber

---
+++ src/EBT/Validator/ValidatorNumber.php
@@ -0,0 +1,43 @@
+<?php
+
+/*
+ * This file is a part of the Validator library.
+ *
+ * (c) 2013 Ebidtech
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace EBT\Validator;
+
+use Symfony\Component\Validator\Constraints\Range as RangeConstraint;
+
+/**
+ * ValidatorNumber
+ */
+abstract class ValidatorNumber extends BaseValidator
+{
+    /**
+     * @param int|float|string $value
+     * @param float            $min
+     * @param float            $max
+     * @param array            $options
+     *
+     * @return bool
+     */
+    public static function inRange($value, $min = null, $max = null, array $options = array())
+    {
+        if ($min !== null) {
+            $options['min'] = $min;
+        }
+        if ($max !== null) {
+            $options['max'] = $max;
+        }
+
+        $violations = static::getValidator()->validateValue($value, new RangeConstraint($options));
+        static::setViolations($violations);
+
+        return static::violationsToBool($violations);
+    }
+}
---
+++ tests/EBT/Validator/Tests/ValidatorNumberTest.php
@@ -0,0 +1,44 @@
+<?php
+
+/*
+ * This file is a part of the Validator library.
+ *
+ * (c) 2013 Ebidtech
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace EBT\Validator\Tests;
+
+use EBT\Validator\ValidatorNumber;
+
+/**
+ * ValidatorNumberTest
+ */
+class ValidatorNumberTest extends TestCase
+{
+    /**
+     * @param mixed     $value
+     * @param int|float $min
+     * @param int|float $max
+     * @param bool      $expected
+     *
+     * @dataProvider providerRanges
+     */
+    public function testInRange($value, $min, $max, $expected)
+    {
+        $this->assertEquals($expected, ValidatorNumber::inRange($value, $min, $max));
+    }
+
+    /**
+     * @return array
+     */
+    public function providerRanges()
+    {
+        return array(
+            // value, min, max, expected,
+            array(10, 5, 30, true)
+        );
+    }
+}
