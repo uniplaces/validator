869c3983e7391ab421a50cd464c88b81253d7d34
entering <entering@gmail.com>
2014-01-12 00:07:05 +0000

    Add Validator collection and tests

---
+++ src/EBT/Validator/ValidatorCollection.php
@@ -0,0 +1,78 @@
+<?php
+
+/*
+ * This file is a part of the Validator library.
+ *
+ * (c) 2013 Ebidtech
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace EBT\Validator;
+
+use Symfony\Component\Validator\Constraints\Choice as ChoiceConstraint;
+use Symfony\Component\Validator\Constraints\Count as CountConstraint;
+
+/**
+ * ValidatorCollection
+ */
+abstract class ValidatorCollection extends BaseValidator
+{
+    /**
+     * @param mixed $value
+     * @param array $choices
+     * @param array $options
+     *
+     * @return bool
+     */
+    public static function inChoice($value, array $choices, array $options = array())
+    {
+        $options['choices'] = $choices;
+
+        $violations = static::getValidator()->validateValue($value, new ChoiceConstraint($options));
+        static::setViolations($violations);
+
+        return static::violationsToBool($violations);
+    }
+
+    /**
+     * @param mixed $value
+     * @param mixed $callback
+     * @param array $options
+     *
+     * @return bool
+     */
+    public static function inChoiceCallback($value, $callback, array $options = array())
+    {
+        $options['callback'] = $callback;
+
+        $violations = static::getValidator()->validateValue($value, new ChoiceConstraint($options));
+        static::setViolations($violations);
+
+        return static::violationsToBool($violations);
+    }
+
+    /**
+     * @param array|object $value
+     * @param int          $min
+     * @param int          $max
+     * @param array        $options
+     *
+     * @return bool
+     */
+    public static function inCount($value, $min = null, $max = null, array $options = array())
+    {
+        if ($min !== null) {
+            $options['min'] = $min;
+        }
+        if ($max !== null) {
+            $options['max'] = $max;
+        }
+
+        $violations = static::getValidator()->validateValue($value, new CountConstraint($options));
+        static::setViolations($violations);
+
+        return static::violationsToBool($violations);
+    }
+}
---
+++ tests/EBT/Validator/Tests/ValidatorCollectionTest.php
@@ -0,0 +1,94 @@
+<?php
+
+/*
+ * This file is a part of the Validator library.
+ *
+ * (c) 2013 Ebidtech
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace EBT\Validator\Tests;
+
+use EBT\Validator\ValidatorCollection;
+
+/**
+ * ValidatorCollectionTest
+ */
+class ValidatorCollectionTest extends TestCase
+{
+    /**
+     * @param mixed $value
+     * @param array $choices
+     * @param bool  $expected
+     *
+     * @dataProvider providerChoices
+     */
+    public function testInChoice($value, array $choices, $expected)
+    {
+        $this->assertEquals($expected, ValidatorCollection::inChoice($value, $choices));
+    }
+
+    /**
+     * @return array
+     */
+    public function providerChoices()
+    {
+        return array(
+            // value, choices, expected
+            array(10, array(10, 20), true),
+            array(5, array(10, 20), false),
+            array(5, array(10, 20), false),
+        );
+    }
+
+    public function testInChoiceCallback()
+    {
+        $this->assertTrue(
+            ValidatorCollection::inChoiceCallback(
+                10,
+                function () {
+                    return array(10, 20);
+                }
+            )
+        );
+
+        $this->assertFalse(
+            ValidatorCollection::inChoiceCallback(
+                10,
+                function () {
+                    return array(30, 20);
+                }
+            )
+        );
+    }
+
+    /**
+     * @param mixed $value
+     * @param int   $min
+     * @param int   $max
+     * @param bool  $expected
+     *
+     * @dataProvider providerCounts
+     */
+    public function testInCount($value, $min, $max, $expected)
+    {
+        $this->assertEquals($expected, ValidatorCollection::inCount($value, $min, $max));
+    }
+
+    /**
+     * @return array
+     */
+    public function providerCounts()
+    {
+        return array(
+            // value, min, max, expected
+            array(array(1, 2, 3), 2, null, true),
+            array(array(1, 2, 3), null, 5, true),
+            array(array(1, 2, 3), 2, 5, true),
+            array(array(1, 2, 3), 5, null, false),
+            array(array(1, 2, 3), 1, 2, false)
+        );
+    }
+}
