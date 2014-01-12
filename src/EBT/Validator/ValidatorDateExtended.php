d72b7641f25a34ef11130fb393850651427011fb
entering <entering@gmail.com>
2014-01-12 11:45:46 +0000

    Add empty ValidatorDateExtended

---
+++ src/EBT/Validator/ValidatorDateExtended.php
@@ -0,0 +1,19 @@
+<?php
+
+/*
+ * This file is a part of the Validator library.
+ *
+ * (c) 2013 Ebidtech
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace EBT\Validator;
+
+/**
+ * ValidatorDateExtended, add convenient methods/alias on top of Symfony validation.
+ */
+abstract class ValidatorDateExtended extends ValidatorDate
+{
+}
